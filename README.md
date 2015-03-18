e2e11179fca55963486d47774d64dcc00933c0e1
Mathieu Martin <mathieu.martin@lightspeedretail.com>
2015-03-18 09:02:05 -0400

    Implement the credentials app a little bit

---
+++ README.md
@@ -0,0 +1,5 @@
+Dependencies
+
+- `whois` package for mkpasswd (Linux passwords)
+- `apache2-utils` for htpasswd (HTTP Auth)
+- `mysql-server` for MySQL password hashes
--- credentials.rb
+++ credentials.rb
@@ -1,4 +1,5 @@
-require 'sinatra'
+require 'shellwords'  # To escape passwords passed to the command line
+require 'sinatra'     # Minimal web framework
 
 get '/' do
   erb :index
@@ -19,9 +20,15 @@ def mysql_encode(password)
 end
 
 def linux_encode(password)
-  'xxx'
+  `mkpasswd -m sha-512 #{Shellwords.escape(password)} 2>/dev/null`.chomp
+  $?.exitstatus == 0 ? hash : '(mkpasswd not available)'
+rescue Errno::ENOENT # Only failed this way once (on OSX). Not sure why.
+  '(mkpasswd not available)'
 end
 
 def http_auth_encode(password)
-  'zzz'
+  hash = `htpasswd -nbB user #{Shellwords.escape(password)} 2>/dev/null`.chomp.split(':').last
+  $?.exitstatus == 0 ? hash : "(htpasswd not available, or version doesn't support bcrypt)"
 end
+
+def mydb
