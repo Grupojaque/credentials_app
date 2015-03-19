1f80b150aa71f3877f40e65c9c3e97367921467a
Mathieu Martin <mathieu.martin@lightspeedretail.com>
2015-03-18 21:07:28 -0400

    First working implementation

--- credentials.rb
+++ credentials.rb
@@ -1,5 +1,11 @@
+# stdlib
+require 'pathname'
 require 'shellwords'  # To escape passwords passed to the command line
+require 'json'
+
+# gems
 require 'sinatra'     # Minimal web framework
+require 'mysql2'
 
 get '/' do
   erb :index
@@ -15,12 +21,14 @@ post '/encode' do
   erb :encode, :locals => { hashes: hashes }
 end
 
+# Encoders
+
 def mysql_encode(password)
-  'yyy'
+  mysql.query(%[SELECT PASSWORD('#{mysql_escape_password(password)}')]).first.values.first
 end
 
 def linux_encode(password)
-  `mkpasswd -m sha-512 #{Shellwords.escape(password)} 2>/dev/null`.chomp
+  hash = `mkpasswd -m sha-512 #{Shellwords.escape(password)} 2>/dev/null`.chomp
   $?.exitstatus == 0 ? hash : '(mkpasswd not available)'
 rescue Errno::ENOENT # Only failed this way once (on OSX). Not sure why.
   '(mkpasswd not available)'
@@ -29,4 +37,22 @@ end
 def http_auth_encode(password)
   hash = `htpasswd -nbB user #{Shellwords.escape(password)} 2>/dev/null`.chomp.split(':').last
   $?.exitstatus == 0 ? hash : "(htpasswd not available, or version doesn't support bcrypt)"
+end
+
+# Helpers
+
+def mysql
+  return @mysql if @mysql
+
+  myconfig = Pathname.new('~/.my.cnf').expand_path
+  if myconfig.exist?
+    @mysql = Mysql2::Client.new(:default_file => myconfig.to_s)
+  else
+    raise "MySQL login configuration file not found"
+  end
+  @mysql
+end
+
+def mysql_escape_password(password)
+  password.gsub("'", "\\'")
 end
